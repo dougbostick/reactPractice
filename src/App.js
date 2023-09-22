2cec4bb0c586fdf16294c256846786b814611d8e
Doug Bostick <dougbostick923@gmail.com>
2023-09-22 14:26:47 -0400

    react practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+  const [userList, setUserList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserList([...userList, [last, first]]);
+    setFirst('');
+    setLast('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Form</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={first} onChange={(e) => setFirst(e.target.value)} />
+        <input value={last} onChange={(e) => setLast(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {userList.map((user) => {
+          return (
+            <div>
+              {user[0]} {user[1]}{' '}
+            </div>
+          );
+        })}
+      </div>
+      <table>
+        <column>First</column>
+        <column>Last</column>
+
+        {/* <row>First</row>
+        <row>Last</row> */}
+      </table>
     </div>
   );
 }
