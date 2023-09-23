a530dac8f0bf34e18d44aae6f303f5c958168028
Doug Bostick <dougbostick923@gmail.com>
2023-09-23 16:43:53 -0400

    adding more to form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { useState } from 'react';
 function App() {
   const [first, setFirst] = useState('');
   const [last, setLast] = useState('');
+  const [email, setEmail] = useState('');
   const [userList, setUserList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setUserList([...userList, [first, last]]);
+    setUserList([...userList, { first, last, email }]);
     setFirst('');
     setLast('');
+    setEmail('');
+  };
+
+  const handleRemove = (target) => {
+    console.log('target', target);
+    setUserList(userList.filter((user) => user.email !== target.email));
   };
 
   return (
@@ -27,24 +34,33 @@ function App() {
           value={last}
           onChange={(e) => setLast(e.target.value)}
         />
+        <input
+          placeholder={'email...'}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
       <table>
         <row>
           <column>First</column>
           <column>Last</column>
+          <column>Email</column>
+
+          <column>Delete</column>
         </row>
         {userList.map((user) => {
           return (
             <row>
-              <column>{user[0]}</column>
-              <column>{user[1]}</column>
+              <column>{user.first}</column>
+              <column>{user.last}</column>
+              <column>{user.email}</column>
+              <column>
+                <button onClick={() => handleRemove(user)}>Remove</button>
+              </column>
             </row>
           );
         })}
-
-        {/* <row>First</row>
-        <row>Last</row> */}
       </table>
     </div>
   );
